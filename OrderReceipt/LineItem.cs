e2cdf7b0da6511e5e896a1eea552340adac88a1b
Chen Xu <xuchen@thoughtworks.com>
2020-11-06 01:07:01 +0800

    refactor: rename Price

--- OrderReceipt/LineItem.cs
+++ OrderReceipt/LineItem.cs
@@ -4,22 +4,18 @@ namespace OrderReceipt
 {
     public class LineItem
     {
-        private double p;
         private int qty;
 
-        public LineItem(string description, double p, int qty)
+        public LineItem(string description, double price, int qty)
         {
             this.Description = description;
-            this.p = p;
+            this.Price = price;
             this.qty = qty;
         }
 
         public string Description { get; }
 
-        public double Price
-        {
-            get { return p; }
-        }
+        public double Price { get; }
 
         public int Quantity
         {
@@ -28,7 +24,7 @@ namespace OrderReceipt
 
         public double TotalAmount
         {
-            get { return p * qty; }
+            get { return Price * qty; }
         }
     }
 }
